360c30c72af0a0aecf3e6d229cc667b89a345a99
ZeroLog <YamaOG3@hotmail.com>
2024-01-29 04:30:02 +0700

    Test 4

--- TestCalc/TestCalc.py
+++ TestCalc/TestCalc.py
@@ -1,4 +1,4 @@
-import unittest
+﻿import unittest
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -33,7 +33,6 @@ class TestOnlineSchool(unittest.TestCase):
         self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="oms_center_block"]/div[4]/a[2]'))).click()
         self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="oms_kilotonne"]'))).send_keys(1)
         element = self.wait.until(EC.visibility_of_element_located((By.ID, 'oms_tonne')))
-        self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="oms_center_block"]/div[4]/a[2]'))).click()
         value = int(element.get_attribute('value'))
         if value == 1000:
             self.to_file("Test 1 passed")
@@ -53,14 +52,13 @@ class TestOnlineSchool(unittest.TestCase):
         result_element = self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="res_main_t"]/center/div')))
         result_text = result_element.text
         last_digit = result_text[-1]
-
-        if last_digit == 1000:
+        if last_digit == "1":
             self.to_file("Test 2 passed")
         else:
             self.to_file("Test 2 failed")
  #standart calc           
     def test3(self):
-        self.to_file("Test 1 start")
+        self.to_file("Test 3 start")
         self.driver.get(self.sitetest) 
         self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="oms_rgh1t"]/div[1]/table/tbody/tr[3]/td[2]/input'))).click()
         self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="oms_rgh1t"]/div[1]/table/tbody/tr[6]/td[3]/input'))).click()
@@ -68,11 +66,29 @@ class TestOnlineSchool(unittest.TestCase):
         self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="oms_rgh1t"]/div[1]/table/tbody/tr[6]/td[4]/input'))).click()
         element=self.wait.until(EC.visibility_of_element_located((By.ID, 'oms_calc_res')))
         value = element.text
-        print(value)
         if value =="16":
-            self.to_file("Test 1 passed")
+            self.to_file("Test 3 passed")
         else:
-            self.to_file("Test 1 failed")
+            self.to_file("Test 3 failed")
+#search test
+    def test4(self):
+        self.to_file("Test 4 start")
+        self.driver.get(self.sitetest) 
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="oms_body1"]/header/nav/div[1]/div/div[1]/div/div/div'))).click()
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="gsc-i-id1"]'))).send_keys("Минор и алгебраическое дополнение матрицы.")
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="___gcse_0"]/div/form/table/tbody/tr/td[2]/button'))).click()
+        time.sleep(1)
+        element = self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="___gcse_1"]/div/div/div/div[5]/div[2]/div/div/div[1]/div[1]/div/div[1]/div/a/b')))
+        value=element.text
+        print(value)
+        if value =="Минор и алгебраическое дополнение матрицы":
+            self.to_file("Test 4 passed")
+        else:
+            self.to_file("Test 4 failed")
+            
+        
+
+        
 
 
     
@@ -85,5 +101,6 @@ if __name__ == "__main__":
     suite.addTest(TestOnlineSchool('test1'))
     suite.addTest(TestOnlineSchool('test2'))
     suite.addTest(TestOnlineSchool('test3'))
+    suite.addTest(TestOnlineSchool('test4'))
 
     unittest.TextTestRunner().run(suite)
